08d4644bbf58ef54b32c1c096bfca871763c7037
tMazzocco <theo.mazzocco@ifts-sls.com>
2026-05-21 12:37:16 +0200

    Implement Explosion and ExplosionBuilder
    ExplosionBuilder can create Explosion with a specific set of particles.

---
+++ EvaluationDesignPatterns/ExplosionBuilder.cpp
@@ -0,0 +1,91 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Particle.cpp"
+
+using namespace std;
+
+class Explosion
+{
+public:
+	string spread;
+    vector<Particle> particles_;
+};
+
+class IExplosionBuilder
+{
+public:
+	virtual ~IExplosionBuilder() = default;
+
+	virtual void withPosition() const = 0;
+	virtual void withColor() const = 0;
+	virtual void withLifetime() const = 0;
+	virtual void withSpread() const = 0;
+	virtual void withCount() const = 0;
+
+	virtual void build() const = 0;
+};
+
+class ClassicExplosionBuilder : public IExplosionBuilder
+{
+private:
+	Explosion* explosionObj;
+	Particle pBlueprint;
+	int count = 0;
+
+public:
+	ClassicExplosionBuilder()
+	{
+		this->Reset();
+	}
+	~ClassicExplosionBuilder()
+	{
+		delete explosionObj;
+	}
+
+	void Reset()
+	{
+		this->explosionObj = new Explosion();
+	}
+
+	void withPosition(Position p)
+	{
+		pBlueprint.position = p;
+	}
+		
+	void withColor(string color)
+	{
+		pBlueprint.color = color;
+	}
+
+	void withLifetime(int lifetime)
+	{
+		pBlueprint.lifetime = lifetime;
+	}
+
+	void withSpread(string spread)
+	{
+		this->explosionObj->spread = spread;
+	}
+
+	void withCount(int count)
+	{
+		this->count = count;
+	}
+
+	virtual void build()
+	{
+		for (int i = 0; i < count; i++)
+		{
+			// Faux mais je n'arrive pas a l'implémenter avec le .clone()
+		    explosionObj->particles_.push_back(pBlueprint);
+		}
+	}
+
+	Explosion* GetExplosion() {
+		Explosion* epxlo = this->explosionObj;
+		this->Reset();
+		return epxlo;
+	}
+};
