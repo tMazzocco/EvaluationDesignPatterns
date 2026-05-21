85c6089e3ad4c12e8533fea26ef8e7c5959a5e42
tMazzocco <theo.mazzocco@ifts-sls.com>
2026-05-21 12:02:18 +0200

    2. Prototype
    Prototyping Paticle to enable the clone() method

--- EvaluationDesignPatterns/Particle.cpp
+++ EvaluationDesignPatterns/Particle.cpp
@@ -5,17 +5,46 @@
 
 struct Position
 {
+protected:
+	float x = 0;
+	float y = 0;
+	float taille = 0;
+	float vitesse = 0;
+	string color;
+
 public:
-	float x;
-	float y;
+	Position() { }
+
+	Position(string color)
+	{
+		this->color = color;
+	}
 };
 
-class Particle
+class IParticlePrototype
 {
-private:
+protected:
+	IParticlePrototype() {}
+	IParticlePrototype(shared_ptr<ParticleFlyweight> pf) {
+		this->graphics = pf;
+		this->position = Position();
+	}
 
 public:
 
 	Position position;
 	shared_ptr<ParticleFlyweight> graphics;
+
+	virtual IParticlePrototype *clone() const = 0;
+};
+
+class Particle : public IParticlePrototype
+{
+private:
+
+public:
+	IParticlePrototype *clone() const override
+	{
+		return new Particle(*this);
+	}
 };
